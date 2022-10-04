8c7c301ed1171d90030c4164d3e48a656cdea554
yankikucuk <kendinikertenkelebek@me.com>
2022-10-04 15:46:49 +0300

    Init

---
+++ BigWigs.lua
@@ -0,0 +1,149 @@
+BigWigs3DB = {
+  ["namespaces"] = {
+    ["BigWigs_Plugins_Victory"] = {
+      ["profiles"] = {
+        ["Default"] = {
+          ["soundName"] = "None",
+        },
+      },
+    },
+    ["BigWigs_Plugins_Alt Power"] = {
+    },
+    ["BigWigs_Plugins_BossBlock"] = {
+    },
+    ["BigWigs_Plugins_Sounds"] = {
+    },
+    ["BigWigs_Plugins_Statistics"] = {
+    },
+    ["BigWigs_Plugins_Countdown"] = {
+      ["profiles"] = {
+        ["Default"] = {
+          ["outline"] = "OUTLINE",
+          ["countdownTime"] = 6,
+          ["fontName"] = "Poppins-Medium",
+          ["voice"] = "English: Overwatch: Tracer",
+        },
+      },
+    },
+    ["BigWigs_Plugins_AltPower"] = {
+    },
+    ["BigWigs_Plugins_Colors"] = {
+      ["profiles"] = {
+        ["Default"] = {
+          ["barEmphasized"] = {
+            ["BigWigs_Plugins_Colors"] = {
+              ["default"] = {
+                nil, -- [1]
+                0.2, -- [2]
+                0.2, -- [3]
+              },
+            },
+          },
+          ["blue"] = {
+            ["BigWigs_Plugins_Colors"] = {
+              ["default"] = {
+                nil, -- [1]
+                nil, -- [2]
+                nil, -- [3]
+                1, -- [4]
+              },
+            },
+          },
+          ["barColor"] = {
+            ["BigWigs_Plugins_Colors"] = {
+              ["default"] = {
+                0.2, -- [1]
+                0.4, -- [2]
+                1, -- [3]
+              },
+            },
+          },
+          ["red"] = {
+            ["BigWigs_Plugins_Colors"] = {
+              ["default"] = {
+                nil, -- [1]
+                nil, -- [2]
+                nil, -- [3]
+                1, -- [4]
+              },
+            },
+          },
+        },
+      },
+    },
+    ["BigWigs_Plugins_Wipe"] = {
+    },
+    ["BigWigs_Plugins_InfoBox"] = {
+      ["profiles"] = {
+        ["Default"] = {
+          ["posx"] = 451,
+          ["posy"] = 716,
+        },
+      },
+    },
+    ["BigWigs_Plugins_Bars"] = {
+      ["profiles"] = {
+        ["Default"] = {
+          ["BigWigsEmphasizeAnchor_y"] = 600,
+          ["fontSize"] = 12,
+          ["BigWigsEmphasizeAnchor_height"] = 11,
+          ["BigWigsAnchor_width"] = 180,
+          ["BigWigsAnchor_y"] = 367,
+          ["fontName"] = "Poppins-Medium",
+          ["BigWigsAnchor_height"] = 10,
+          ["BigWigsEmphasizeAnchor_x"] = 580,
+          ["visibleBarLimit"] = 10,
+          ["BigWigsAnchor_x"] = 1028,
+          ["spacing"] = 18,
+          ["barStyle"] = "AddOnSkins Half-Bar",
+          ["BigWigsEmphasizeAnchor_width"] = 320,
+          ["visibleBarLimitEmph"] = 6,
+          ["texture"] = "ElvUI Blank",
+        },
+      },
+    },
+    ["BigWigs_Plugins_Super Emphasize"] = {
+    },
+    ["LibDualSpec-1.0"] = {
+    },
+    ["BigWigs_Plugins_Proximity"] = {
+      ["profiles"] = {
+        ["Default"] = {
+          ["height"] = 120,
+          ["posy"] = 727,
+          ["posx"] = 332,
+          ["width"] = 139,
+        },
+      },
+    },
+    ["BigWigs_Plugins_Messages"] = {
+      ["profiles"] = {
+        ["Default"] = {
+          ["outline"] = "NONE",
+          ["emphFontName"] = "Poppins-Medium",
+          ["fontName"] = "Poppins-Medium",
+          ["emphOutline"] = "OUTLINE",
+        },
+      },
+    },
+    ["BigWigs_Plugins_AutoReply"] = {
+    },
+    ["BigWigs_Plugins_Pull"] = {
+    },
+    ["BigWigs_Plugins_Raid Icons"] = {
+    },
+  },
+  ["profileKeys"] = {
+    ["Jergum - Silvermoon"] = "Default",
+    ["Faithrien - Silvermoon"] = "Default",
+  },
+  ["profiles"] = {
+    ["Default"] = {
+      ["fakeDBMVersion"] = true,
+    },
+  },
+}
+BigWigsIconDB = {
+}
+BigWigsStatsDB = {
+}
